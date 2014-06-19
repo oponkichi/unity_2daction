672ed5d7b6b7009eb1c29370bab7f3c430ed656d
Norihide Okada <n-okada@inis.co.jp>
2014-06-19 13:37:18 +0900

    [UPDATE]

---
+++ Assets/Scripts/GameContext.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameContext {
+	private static GameContext	ms_Instance = null;
+
+	public static GameContext	Instance()
+	{
+		if( ms_Instance == null )
+		{
+			ms_Instance = new GameContext();
+		}
+		return ms_Instance;
+	}
+
+
+	void Awake() {
+		//DontDestroyOnLoad( this );
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Log( string msg )
+	{
+		Debug.Log( msg );
+	}
+}
---
+++ Assets/Scripts/TitleController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameContext.Instance().Log( "Title Started" );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
