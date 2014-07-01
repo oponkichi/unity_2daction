48b74bee124bd1e1f6bad21fa282a7e72a9b8e4f
Norihide Okada <o.ponkichi@gmail.com>
2014-07-01 14:04:31 +0900

    インゲーム用のスプライトなどのデータを追加

---
+++ Assets/Scripts/Player/ArrowLauncher.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowLauncher : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void FixedUpdate()
+	{
+		if (m_HoldingArrow != null)
+		{
+			if( Input.touchCount == 0 )
+			{
+				Launch();
+			}
+			else
+			{
+			}
+		}
+		else
+		{
+			if( Input.touchCount > 0 )
+			{
+				Spawn();
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	protected void Spawn()
+	{
+	}
+
+	protected void Launch()
+	{
+	}
+
+	GameObject		m_HoldingArrow;
+}
